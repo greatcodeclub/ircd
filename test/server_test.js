f2f7ac77feee019cf049c451fe0f794ea9b7d02a
macournoyer <macournoyer@gmail.com>
2014-05-02 16:11:27 -0400

    Simplify server_test.js

--- test/server_test.js
+++ test/server_test.js
@@ -7,40 +7,39 @@ server.log = function() {} // quiet server
 
 describe('Server', function() {
   function connect(nick, done) {
-    var client = new irc.Client('localhost', nick)
-    client.on('registered', function() {
-      client.join('#test', function() {
-        done()
-      })      
-    })
+    // Use a real IRC client to test the server
+    var client = new irc.Client('localhost', nick, { channels: ['#test'] })
+    client.on('join', _.once(function() {
+      done()
+    }))
     return client
   }
 
   beforeEach(function(done) {
     var done = _.after(2, done)
-    this.client1 = connect('client1', done)
-    this.client2 = connect('client2', done)
+    this.user1 = connect('user1', done)
+    this.user2 = connect('user2', done)
   })
 
   afterEach(function(done) {
     var done = _.after(2, done)
-    this.client1.disconnect(null, done)
-    this.client2.disconnect(null, done)
+    this.user1.disconnect(null, done)
+    this.user2.disconnect(null, done)
   })
 
   it('sends names', function(done) {
-    this.client2.on('names', function(channel, nicks) {
+    this.user2.on('names', function(channel, nicks) {
       assert.equal(channel, '#test')
-      assert.deepEqual(Object.keys(nicks), ['client1', 'client2'])
+      assert.deepEqual(Object.keys(nicks), ['user1', 'user2'])
       done()
     })
   })
 
   it('sends message', function (done) {
-    this.client1.say('#test', 'Hi there')
+    this.user1.say('#test', 'Hi there')
 
-    this.client2.on('message#test', function(nick, text, message) {
-      assert.equal(nick, 'client1')
+    this.user2.on('message#test', function(nick, text, message) {
+      assert.equal(nick, 'user1')
       assert.equal(text, 'Hi there')
       done()
     })
