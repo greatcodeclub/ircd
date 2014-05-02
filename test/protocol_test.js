a4e46c5ab2a340fbcffa9018379667d7556edd03
macournoyer <macournoyer@gmail.com>
2014-05-02 13:31:12 -0400

    Remove USER command code.

--- test/protocol_test.js
+++ test/protocol_test.js
@@ -1,25 +1,16 @@
 var assert = require('assert'),
-    mocks = require('./mocks'),
-    protocol = require('../lib/protocol')
+    protocol = require('../lib/protocol'),
+    User = require('../lib/user').User
 
 describe('Protocol', function() {
   beforeEach(function () {
-    this.server = mocks.server()
-    this.user = mocks.user('ma')
+    this.server = {}
+    this.user = new User()
   })
 
   it('handles NICK', function () {
     protocol.NICK(this.server, this.user, { nick: 'test' })
     
     assert.equal(this.user.nick, 'test')
-  })
-  
-  it('handles USER', function () {
-    protocol.USER(this.server, this.user, { hostname: 'hostname' })
-    
-    assert.equal(this.user.connection.sent,
-                 ":test.local 001 ma :Welcome\r\n" +
-                 ":test.local 002 ma :Your host is hostname, running version ircd-1.0\r\n" +
-                 ":test.local 376 ma :End of /MOTD command.\r\n")
   })
 })
