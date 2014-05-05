9b22ea56d541c5e8768b59733d407366b77227b4
macournoyer <macournoyer@gmail.com>
2014-05-05 14:56:36 -0400

    Add tests.

---
+++ test/user_test.js
@@ -0,0 +1,15 @@
+var assert = require('assert'),
+    mocks = require('./mocks')
+
+describe('User', function() {
+  beforeEach(function () {
+    this.user = mocks.user('ma')
+  })
+
+  it('sends reply', function () {
+    this.user.sendReply("001", ":Welcome")
+
+    assert.deepEqual(this.user.connection.sent,
+                     ":test.local 001 ma :Welcome\r\n")
+  })
+})
