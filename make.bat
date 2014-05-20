197ae0b27f8fdeb713d53494a0aaf29855377778
macournoyer <macournoyer@gmail.com>
2014-05-20 16:24:28 -0400

    Add quotes around bin paths in make.bat.

--- make.bat
+++ make.bat
@@ -4,15 +4,15 @@ REM This batch file mimics what the Makefile does on Windows
 
 FOR /F "tokens=*" %%i IN ('npm bin') DO SET BIN=%%i
 
-%BIN%\jison lib\parser.jison -o lib\parser.js
+"%BIN%\jison" lib\parser.jison -o lib\parser.js
 
 if "%1" == "test" (
-  %BIN%\mocha --reporter spec
+  "%BIN%\mocha" --reporter spec
   goto DONE
 )
 
 if "%1" == "watch" (
-  %BIN%\nodemon -x 'make test' -e 'js jison' -q
+  "%BIN%\nodemon" -x 'make test' -e 'js jison' -q
   goto DONE
 )
 
